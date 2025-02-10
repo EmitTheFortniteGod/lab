c8e469669b89e6a3c07678dbf047179d2d5218d1
EmitTheFortniteGod <147779921+EmitTheFortniteGod@users.noreply.github.com>
2025-02-10 14:31:08 +1100

    Update lab.py

---
+++ lab.py
@@ -0,0 +1,6 @@
+animal = input("Give me an animal")
+verb = input("Give me a verb ending in 'ing'")
+noun =input("Give me a place")
+object = input("Give me a singular object")
+
+print (animal, "went", verb, "to",noun , "to get a", object)
