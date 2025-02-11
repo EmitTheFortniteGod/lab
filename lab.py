01909357a3ddd4edeb73d205a64dc9a6eb9cb742
EmitTheFortniteGod <147779921+EmitTheFortniteGod@users.noreply.github.com>
2025-02-11 14:07:57 +1100

    bill

--- lab.py
+++ lab.py
@@ -1,6 +1,7 @@
-animal = input("Give me an animal")
-verb = input("Give me a verb ending in 'ing'")
-noun =input("Give me a place")
-object = input("Give me a singular object")
+animal = input("Give me an Animal ").title().strip()
+verb = input("Give me a verb ending in 'ing' ").strip()
+noun =input("Give me a Place ").title().strip()
+object = input("Give me a singular object ").title().strip()
+object2 = input("Give me a singular object ").title().strip()
 
-print (animal, "went", verb, "to",noun , "to get a", object)
+print  (f"A {animal} went {verb} to {noun} to get a {object}. After you got ")
---
+++ reportbot.py
@@ -0,0 +1,2 @@
+Student_Name = input ("Whats your students full name ")
+Student_year = input ("what is your students Year Level ")
